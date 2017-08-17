2dfcd6b66b5ea1b1389c682e397b5cf2cd136bf8
BFunk <bret.e.funk@gmail.com>
2017-08-16 21:46:44 -0600

    fixed serializer bugs

--- app/controllers/api/v1/invoice_items/invoice_controller.rb
+++ app/controllers/api/v1/invoice_items/invoice_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::InvoiceItems::InvoiceController < ApplicationController
+class Api::V1::InvoiceItems::InvoicesController < ApplicationController
 
   def show
     render json: Invoice.find(params[:invoice_id])
